8ffddfd869e1abce904ae8220719ed84224d402f
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2017-07-07 00:05:08 +0200

    Created a nameday app featuring nameday's info and filtering.

---
+++ Nameday/NamedayModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameday
+{
+    public class NamedayModel
+    {
+        public int Month { get; }
+        public int Day { get; }
+        public IEnumerable<string> Names { get; }
+        public string NamesAsString => string.Join(", ", Names);
+
+        public NamedayModel(int month, int day, IEnumerable<string> names)
+        {
+            Month = month;
+            Day = day;
+            Names = names;
+        }
+    }
+}
